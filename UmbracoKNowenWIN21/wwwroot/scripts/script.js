242d207e67463fa077206a1407f2dc0d1dfda878
Kristoffer Nowén <kristoffer.nowen@learnet.se>
2023-01-18 17:31:09 +0100

    Add fixed footer in script tag, script.js not working, changeChange work in script but not in script.js so there is something wrong

---
+++ UmbracoKNowenWIN21/wwwroot/scripts/script.js
@@ -0,0 +1,7 @@
+function changeChange() {
+    if (document.getElementById('changeHeader').style.color === 'red') {
+        document.getElementById('changeHeader').style.color = 'black'
+    } else {
+        document.getElementById('changeHeader').style.color = 'red'
+    }
+}
