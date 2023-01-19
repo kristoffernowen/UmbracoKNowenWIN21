9a2c66f6e912e3a67cfd7b6a6633ba914201f031
Kristoffer Nowén <kristoffer.nowen@learnet.se>
2023-01-19 15:43:24 +0100

    Add aspappend version to script, remove script tags and change header and button

--- UmbracoKNowenWIN21/wwwroot/scripts/script.js
+++ UmbracoKNowenWIN21/wwwroot/scripts/script.js
@@ -1,7 +1,28 @@
-function changeChange() {
-    if (document.getElementById('changeHeader').style.color === 'red') {
-        document.getElementById('changeHeader').style.color = 'black'
-    } else {
-        document.getElementById('changeHeader').style.color = 'red'
-    }
+
+
+if (document.body.clientHeight < window.innerHeight) {
+    document.querySelector('.footer').style.position = 'fixed'
 }
+
+/*
+
+let pagePosition = window.screenY
+
+const navbar = document.getElementById("myNavbar")
+const whiteOnScroll = () => navbar.classList.add("bg-white")
+const greyOnTop = () => navbar.classList.remove("bg-white")
+
+window.addEventListener('scroll', () => {
+    pagePosition = window.scrollY
+
+    if (pagePosition >= 1) {
+        whiteOnScroll
+    }
+    else {
+        greyOnTop
+    }
+})
+
+    works in vs code but something is wrong here
+
+*/
