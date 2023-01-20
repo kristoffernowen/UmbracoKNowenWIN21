a484c229ec9203cc13aab3220318014a23679a33
kristoffernowen <89974843+kristoffernowen@users.noreply.github.com>
2023-01-20 13:52:42 +0100

    Fixes (#6)
    * Add fixed navbar, navbar-compensator hack
    
    * Add fixed footer in script tag, script.js not working, changeChange work in script but not in script.js so there is something wrong
    
    * add a link to productUm, works but is partially disturbed by something
    
    * Add exclude product from navbar links in selection
    
    * corrected script src for script.js
    
    * Add aspappend version to script, remove script tags and change header and button
    
    * Add dynamic rating to productUm, a link on product name

--- UmbracoKNowenWIN21/wwwroot/scripts/script.js
+++ UmbracoKNowenWIN21/wwwroot/scripts/script.js
@@ -1,1 +1,28 @@
-﻿
+
+
+if (document.body.clientHeight < window.innerHeight) {
+    document.querySelector('.footer').style.position = 'fixed'
+}
+
+/*
+
+let pagePosition = window.screenY
+
+const navbar = document.getElementById("myNavbar")
+const whiteOnScroll = () => navbar.classList.add("bg-white")
+const greyOnTop = () => navbar.classList.remove("bg-white")
+
+window.addEventListener('scroll', () => {
+    pagePosition = window.scrollY
+
+    if (pagePosition >= 1) {
+        whiteOnScroll
+    }
+    else {
+        greyOnTop
+    }
+})
+
+    works in vs code but something is wrong here
+
+*/
